c891679d28d641e56712169fd8d9114b02f48819
mmorit12 <88303545+mmorit12@users.noreply.github.com>
2021-08-02 18:58:53 -0700

    Add files via upload

---
+++ mark.php
@@ -0,0 +1,18 @@
+<html>
+ <head>
+  <title>
+    Things About Me
+  </title>
+ </head>
+  <body>
+   <?php
+     echo " <h1><center> About Me!<br><br><br> </center></h1>";
+     echo "Hello My name is Mark Louie";
+     echo "<br>";
+     echo "I'm a 4th year student in University of Makati";
+     echo "<br>";
+     echo "I live in Makati"; 
+     echo "<br>";
+   ?>
+  </body>
+</html>
