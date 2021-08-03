b75a9e3b74ee3853770e5d9ecd7968762de14893
mmorit12 <88303545+mmorit12@users.noreply.github.com>
2021-08-02 18:59:25 -0700

    Update mark.php

--- mark.php
+++ mark.php
@@ -6,7 +6,7 @@
  </head>
   <body>
    <?php
-     echo " <h1><center> About Me!<br><br><br> </center></h1>";
+     echo " <h1><center> About Me<br><br><br> </center></h1>";
      echo "Hello My name is Mark Louie";
      echo "<br>";
      echo "I'm a 4th year student in University of Makati";
